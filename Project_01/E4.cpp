3919c3a79e42b1b9251ce9f0c1148d058d5cf44c
WilliamNoside <ghs0156020@126.com>
2024-10-17 23:03:20 +0800

    10/17 日 E4 修改 加注释

--- Project_01/E4.cpp
+++ Project_01/E4.cpp
@@ -9,7 +9,7 @@
                                 \\_V_//
           捉虫勇士               \/=|=\/
           BUG死光光               [=v=]
-                               __\___/_____
+          代码和圣骑士一般坚韧    __\___/_____
                               /..[  _____  ]
                              /_  [ [  M /] ]
                             /../.[ [ M /@] ]
@@ -41,17 +41,31 @@
     - 名字的空格只有一个。
 
 The usage of scanf, cin and subscript are forbidden.
-The usage of assign, find and append must be present.i
+The usage of assign, find and append must be present.
 
+思路
+- 输入姓名，格式为“Firstname Lastname”
+- 按空格分割字符串，并存储到 vector 中
+- 判断 vector 的大小，如果小于 2，则询问是否有姓氏，如果没有，则输出结果并跳出循环
+- 如果 vector 的大小大于 3，则输出错误信息并跳出循环
+- 遍历 vector，判断每个字符串是否只包含字母
+- 记录名字的各个部分
+- 输出结果，如果有中间名，则输出中间名的首字母
     
 */
+
+// 和 is_number 函数一样，只不过是判断字符串是否只包含字母
 bool is_alpha(const std::string& str) {
+
     // 判断字符串是否只包含字母
+
     for (char c : str) {
         if (!std::isalpha(c)) {  // 如果有不是字母的字符，则返回 false
             return false;
         }
     }
+
+    // 全部都是字母，返回 true
     return true;
 }
 
@@ -77,13 +91,15 @@ int main() {
 
         // 输入有效，判断格式是否正确
 
-        std::stringstream ss(full_name);
-        std::vector<std::string> name_parts;
-        std::string part;
+        std::stringstream ss(full_name); // 将字符串分割成多个字符串
+        std::vector<std::string> name_parts; // vector 存储姓名的各个部分
+        std::string part; // 临时变量存储字符串
 
         while (ss >> part) {
             name_parts.push_back(part);
         }
+        
+        // 按空格分割字符串，并存储到 vector 中
 
         if (name_parts.size() < 2 ) {
             std::cout << "Hi," << full_name << ",Nice to meet you! Do you have a surname?(Y/N)" << std::endl;
@@ -102,22 +118,38 @@ int main() {
 
         bool valid = true;
         for (const std::string& name : name_parts) {
+            // 判断字符串是否只包含字母
             if (!is_alpha(name)) {
                 valid = false;
                 break;
             }
+            
         }
 
         if (!valid) {
+            // 输入格式不正确
             std::cout << "Are you kidding me ?! Name should only contain alphabetic characters." << std::endl;
-            continue;  // 重新请求输入
+            continue;  
+            // 重新请求输入
         }
 
-        size_t space_pos = full_name.find(' '); // 记录空格的位置
+        size_t space_pos = full_name.find(' '); 
+
+        // 记录空格的位置
 
         std::string firstname = name_parts[0]; // 名字
         std::string lastname = name_parts[name_parts.size() - 1]; // 姓氏
         std::string middlename = (name_parts.size() == 3) ? name_parts[1] : ""; // 名字中间的名字
+
+        if(firstname.length() > 12 || lastname.length() > 12 || middlename.length() > 12){
+            std::cout << "Your name is too long, are you a alien ?! Please enter a name with less than 12 characters." << std::endl;
+            continue;  // 重新请求输入
+        } else if (firstname.length() < 2 || lastname.length() < 2){
+            std::cout << "Bro think he is a pronumeral. Please enter a name with more than 1 character." << std::endl;
+            continue;  // 重新请求输入
+        }
+        
+        // 记录名字的各个部分
 
         std::string initials;
         initials.append(1, firstname[0]);  // 名字的首字母
@@ -125,6 +157,7 @@ int main() {
             initials.append(1, middlename[0]);  // 中间名的首字母
         }
         initials.append(1, lastname[0]);   // 姓氏的首字母
+
 
         std::cout << "First Name: " << firstname << std::endl;
         if (!middlename.empty()) {
